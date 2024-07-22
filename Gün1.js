b208b085949bf1674a3433af11f69ccf28e91a94
Ayberk <ayberkcetinkaya54@gmail.com>
2024-07-22 11:51:29 +0300

    Commit1

---
+++ Gün1.js
@@ -0,0 +1,31 @@
+/*   Sprint 5 Ders Notları */
+/* React Başladı */
+/* 
+React bir frameworktür.
+Temel faydalarını öğrenecez.
+İlk ders Dom Manipülasyonları ile başlayacaz.
+*/
+
+/* 
+HTML Statik veri göstermek için bir sunum dilidir. Kendiliğinden DEĞİŞMEZ.
+Documentt Object Model (DOM) Browser HTML dosyasını okuyuğ bir JS  objesi oluşturur.
+Sayfadaki değişiklikler bu obje üzerinden yapılır, HTML değiştirmeye gerek kalmaz.
+*/
+
+/*
+Dom, bir web sayfasının canlı, interaktif bir temsilidir ve sayfadaki değişiklikler bu yapı üzerinde gerçekleştirilir.
+*/
+
+/*
+HTML: Ev Planı 
+Dom: Ev'in kendisidir.
+*/
+
+/*
+Her bir HTML tagini onu temsil eden bir objeye dönüştürür.
+Bu objeleri taglerin arasındaki parent-child ilişkisine göre bir ağaç yapısına yerleştirir.
+HTML body gibi elemanlar ağacın ana gövdesine yakın olur. Bunların içindekiler ise ağacın daha üstteki dallarında yer alır
+Dom ağacının kökünde document objesi bukunurç
+Bu objeler DOM ağacındaki yerlerine node , yani düğüm denir.
+Tagler HTML dosyasında bulundukları konuma göre yabş sahip oldukları parent child ilişkilerine göre DOM ağacına yerleştirilir.
+*/
