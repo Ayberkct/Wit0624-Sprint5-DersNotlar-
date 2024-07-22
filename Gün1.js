41fc1c64af1bd82a2719399effc5b811cc58f4a2
Ayberk <ayberkcetinkaya54@gmail.com>
2024-07-22 13:34:47 +0300

    Commit6

--- Gün1.js
+++ Gün1.js
@@ -43,16 +43,33 @@ Tagler HTML dosyasında bulundukları konuma göre yabş sahip oldukları parent
 // js OBJELERİ İÇİNDE DOT OPERATÖRÜ KULLANMAK VERİMSİZ , ÇOK İÇ İÇE ELEMAN OLABNİLİR.
 //document.body.ariaValueMin.section.article.div.div; //   VERİMSİZ YANLIŞ XXXXXXXXX
 
-// Elemanları ödelliklerine göre deçmeliyiz.
-
+// Elemanları özelliklerine göre seçmeliyiz.
 //const Button = document.getElementById("acKapa"); // elemanın id sinden yakaladığmız metot
 //document.getElementsByClassName("acKapa"); // Class name ine göre yakalar
 //document.getElementsByName("acKapa"); // adına göre yakalar.
-//document.getElementsByTagName(p); // tag name ine göre yakalar
+//document.getElementsByTagName("p"); // tag name ine göre yakalar
 
-document.getElementById("acKapa").textContent = "Çetinkaya"; // acKapa id sine ait tagin textini değiştirmek için kullanılır.
+document.getElementById("acKapa").textContent = "Çetinkaya"; // acKapa id sine ait tagin textini değiştirmek için kullanılır.0
+document.getElementById("ad").style.color = "white";
 
 document.querySelector("body").style.backgroundColor = "orange"; //  ===>>>> CSS değiştirmek için önce style.yazılır!!!!!!
 
-document.querySelectorAll(".photos img"); // ===>>> parantez içindeki selectorle eşleşen her elemanı seçer . BİR ARRAY DÖNER.
+document.querySelectorAll(".photos img"); // ===>>> parantez içindeki selectorle eşleşen her elemanı seçer . BİR ARRAY DÖNER.  ( FOTOĞRAFIN ALTINDAKİ İMG LER DEMEKTİR)
 document.querySelector("body"); // ===> parantez içindeki selectorle eşleşen ilk elemanı seçer. TEK BİR DOM OBJESİ DÖNER
+
+const imgs = document.querySelectorAll("img");
+for (let i = 0; i < imgs.length; i++) {
+  //=====> Birden çok döndüğü için bu şekilde for döngüsü kullanabiliriz.
+  imgs[i].style.border = " 2px solid white";
+}
+
+// stilleri grubladığımız zaman yukarıdaki şekilde tek tek yazmayız. Peki bunu nasıl yaparız ???
+const button = document.querySelector("button");
+
+// tek bir class da toplayıp istedğimiz tag lere class olarak ekle, çıkar gibi şeyler yapabilliriz. Bu classın stillerini de css de hazırlarız.
+
+button.classList; //==>> class ları sıralar.
+button.classList.add("siyah"); // ===> class ekler.
+button.classList.remove("siyah"); // ===>> class çıkarır.
+button.classList.toggle("siyah"); //==>> varsa çıkarır yoksa ekler.
+button.classList.contains("siyah"); //===> class içinde varsa true döner yoksa false döner.
